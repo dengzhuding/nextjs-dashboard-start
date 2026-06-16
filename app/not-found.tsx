3f4de3a0d6f06b8a6c751d2c6019f2412825e75d
dzd <dzd@test.com>
2026-06-16 21:53:03 +0800

    feat: chapter 3 - Optimizing Fonts and Images

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-6">
+      <h1 className="text-4xl font-bold">404 test</h1>
+      <p className="text-lg text-gray-600">Page not found.</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,29 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div class={styles.shape}></div>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
+      {/* <div className={styles.shape}></div> */}
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+
           </p>
+          <p className={lusitana.className}>
+            Acme is a fictional company created for demonstration purposes. This
+            dashboard template is designed to showcase the capabilities of Next.js
+            and Tailwind CSS in building modern web applications.
+          </p>
+          
+           <input type="text" name="email" autoComplete="email" className="" placeholder="Enter your email" />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -28,6 +34,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
